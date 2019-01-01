77c31750cfcfba4ebbece8316b8744dd082b2eac
Michael Lo <nz.ampersand@gmail.com>
2019-01-01 13:35:13 +1300

    Add unit test to validate that scraper class exists

--- src/test/scraper/TestTLAScraper.py
+++ src/test/scraper/TestTLAScraper.py
@@ -1,1 +1,11 @@
+import unittest
 
+
+class TestTLAScraper(unittest.TestCase):
+    def class_exists(self):
+        def scraper = TLAScraper()
+        self.assertIsNotNone(scraper)
+
+
+if __name__ == '__main__':
+    unittest.main()
