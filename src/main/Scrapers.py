2827d7babd7e034c1d64223cb5e0ffb1a84e2188
Michael Lo <nz.ampersand@gmail.com>
2019-01-01 14:06:23 +1300

    Implement a couple of helper functions to get table of contents

--- src/main/Scrapers.py
+++ src/main/Scrapers.py
@@ -6,7 +6,24 @@ class Scraper(ABC):
     def scrape(self):
         pass
 
+    @abstractmethod
+    def table_of_contents_url(self):
+        pass
+
+    @abstractmethod
+    def table_of_contents_element_id(self):
+        pass
+
 
 class TLAScraper(Scraper):
+    TOC_URL = "https://forums.spacebattles.com/threads/the-last-angel.244209/"
+    TOC_ELEMENT_ID = "post-9354450"
+
     def scrape(self):
         pass
+
+    def table_of_contents_url(self):
+        return self.TOC_URL
+
+    def table_of_contents_element_id(self):
+        return self.TOC_ELEMENT_ID
--- src/test/TestTLAScraper.py
+++ src/test/TestTLAScraper.py
@@ -8,6 +8,14 @@ class TestTLAScraper(unittest.TestCase):
         scraper = TLAScraper()
         self.assertIsNotNone(scraper)
 
+    def has_toc_url(self):
+        scraper = TLAScraper()
+        self.assertIsNotNone(scraper.table_of_contents_url())
+
+    def has_toc_element_id(self):
+        scraper = TLAScraper()
+        self.assertIsNotNone(scraper.table_of_contents_element_id())
+
 
 if __name__ == '__main__':
     unittest.main()
